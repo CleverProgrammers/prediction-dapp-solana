4b29feff6ff93a9b684d50b727e4aaf88f6fcd51
Strong10mede <kmayur809@gmail.com>
2023-04-16 18:28:09 +0530

    repo init

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import { useEffect, useMemo, useState } from "react";
+import { RPC_ENDPOINT } from "../utils/constants";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,2 @@
-
+export const RPC_ENDPOINT =
+  "https://smart-proportionate-liquid.solana-devnet.discover.quiknode.pro/fdc48fdc6b67c1095bbb37bce6d2b5a6b87722d3/"; //node to connect to the solana blockchain
